b0b20da9bdeb9b827aa3c463e211daba461d55fc
johnrcd <80877409+johnrcd@users.noreply.github.com>
2023-10-09 23:04:32 -0500

    added styling to HomePage

--- src/pages/HomePage/index.jsx
+++ src/pages/HomePage/index.jsx
@@ -1,23 +1,37 @@
 import MainNavigationBar from "../../components/MainNavigationBar";
 import { Outlet, Link } from "react-router-dom";
+import "./styles.css";
 
 function HomePage(){
     return(
         <>
             <MainNavigationBar />
-            <div>
-                <p>Hi, my name is</p>
-                <h1>Rovi Decena</h1>
-                <h2>I write code for apps, websites, and games.</h2>
-                <h3>Also I make music, sometimes.</h3>
-                <p>
-                    Currently, I'm taking the Business Information Technology 
-                    course at Red River Polytech.
-                </p>
-            </div>
-            <div>
-                <p>Interested in seeing some of my work?</p>
-                <Link to="/projects">Check it out here.</Link>
+            <div className="titleCardWrapper">
+            <section className="titleCard">
+                <article className="main">
+                    <p>Hey, my name is</p>
+                    <h1>Rovi Decena</h1>
+                    <h2>I write code for apps, websites, and games.</h2>
+                    <h3>Also I make music, sometimes.</h3>
+                </article>
+                <article className="bio">
+                    <p>
+                        I'm a software developer with a focus on application
+                        and game development. I first got into programming
+                        through an Interactive Digital Media program.
+                    </p>
+                    <br/>
+                    <p>
+                        Currently, I'm taking the Business Information
+                        Technology program over at Red River College
+                        Polytechic.
+                    </p>
+                </article>
+                <article className="showcase">
+                    <p>Interested in seeing some of my work?</p>
+                    <Link to="/projects">Check it out here.</Link>
+                </article>
+            </section>
             </div>
         </>
     )
