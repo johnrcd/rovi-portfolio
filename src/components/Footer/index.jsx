d5160281e859e802ee126b273fd648b3a27d3d8a
johnrcd <80877409+johnrcd@users.noreply.github.com>
2023-12-07 02:21:56 -0600

    credited Iconfinder in footer

--- src/components/Footer/index.jsx
+++ src/components/Footer/index.jsx
@@ -6,6 +6,7 @@ export default function Footer(){
             <p>
                 Built with <a href="https://react.dev/">React</a> and <a href="https://vitejs.dev/">Vite</a>.
                 Styled with the help of <a href="https://andybrewer.github.io/mvp/">MVP.css</a>.
+                Icons from <a href="https://www.iconfinder.com/">Iconfinder</a>.
             </p>
         </footer>
     );
